95b941a32b54dccd50390c6805f6d015d9154ddf
SKRINTRAP <163513483+Baastan@users.noreply.github.com>
2024-04-22 21:05:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function Fun1 () {
+    let a = Number(prompt('напиши первое ЧИСЛО'))
+    let b = Number(prompt('напиши второе ЧИСЛО'))
+    let c = a + b
+    document.getElementById('asd').innerHTML = ( a + '+' + b + '=' + c)
+}
+function Fun2 () {
+    let a = Number(prompt('напиши первое ЧИСЛО'))
+    let b = Number(prompt('напиши второе ЧИСЛО'))
+    let c = a - b
+    document.getElementById('asd').innerHTML = ( a + '-' + b + '=' + c)
+}
+function Fun3 () {
+    let a = Number(prompt('напиши первое ЧИСЛО и нажми *ок*'))
+    let b = Number(prompt('напиши второе ЧИСЛО и нажми *ок*'))
+    let c = a * b
+    document.getElementById('asd').innerHTML =  ( a + '*' + b + '=' + c)
+}
+function Fun4 () {
+    let a = Number(prompt('напиши первое ЧИСЛО'))
+    let b = Number(prompt('напиши второе ЧИСЛО'))
+    let c = a / b
+    document.getElementById('asd').innerHTML =  ( a + '/' + b + '=' + c)
+}
+
+
+function fun5 () {
+    let a = document.body.style.backgroundColor = 'darkslategrey'
+}
+
+function fun7 () {
+    let a = document.body.style.backgroundColor = 'black'
+}
+
+function fun6 () {
+    let a = document.body.style.backgroundColor = 'wheat'
+}
